743db5bf46cbe58779931b402363a018894bce57
Sahitya Kushwaha <83616018+sahitya1903@users.noreply.github.com>
2025-08-21 22:46:42 +0530

    typo

--- Queue/queuebydeque.java
+++ Queue/queuebydeque.java
@@ -2,27 +2,27 @@ import java.util.Deque;
 import java.util.LinkedList;
 
 public class queuebydeque {
-       static class Queue{
-    static Deque<Integer> deque=new LinkedList<>();
+    static class Queue{
+        static Deque<Integer> deque=new LinkedList<>();
 
-    public boolean isEmpty() {
-        return deque.size()==0;
-    }
-    
-    public void add(int data) {
-            deque.addLast(data);
-    }
+        public boolean isEmpty() {
+            return deque.size()==0;
+        }
+        
+        public void add(int data) {
+                deque.addLast(data);
+        }
 
-    public int remove() {
-        int a=deque.removeFirst();
-        return a;
-    }
+        public int remove() {
+            int a=deque.removeFirst();
+            return a;
+        }
 
-    public int peek(){
-        int a=deque.getFirst();
-        return a;
+        public int peek(){
+            int a=deque.getFirst();
+            return a;
+        }
     }
-
     public static void main(String[] args) {
         Queue q=new Queue();
         q.add(1);
@@ -35,5 +35,4 @@ public class queuebydeque {
             q.remove();
         }
     }
-}
 }
