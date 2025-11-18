edab43be9e48b8319277506a87f6b89752409819
Sahitya Kushwaha <83616018+sahitya1903@users.noreply.github.com>
2025-11-18 14:28:46 +0530

    Add custom exception and demonstration

---
+++ Practice/CustomExcProgram.java
@@ -0,0 +1,15 @@
+class AgeException extends Exception {
+    public AgeException(String message) {
+        super(message);
+    }
+}
+
+public class CustomExcProgram {
+    public static void main(String[] args) {
+        try {
+            throw new AgeException("Age must be 18 or above to vote.");
+        } catch (AgeException e) {
+            System.out.println("Caught: " + e.getMessage());
+        }
+    }
+}
