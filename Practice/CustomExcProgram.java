84c4c2d6ee048695f2c58fd33fa4668831a3822e
Sahitya Kushwaha <83616018+sahitya1903@users.noreply.github.com>
2025-11-30 11:25:36 +0530

    Refactor CustomExcProgram to use ArithmeticException and remove AgeException

--- Practice/CustomExcProgram.java
+++ Practice/CustomExcProgram.java
@@ -1,14 +1,26 @@
-class AgeException extends Exception {
-    public AgeException(String message) {
-        super(message);
-    }
-}
+// class AgeException extends Exception {
+//     public AgeException(String message) {
+//         super(message);
+//     }
+// }
+
+// public class CustomExcProgram {
+//     public static void main(String[] args) {
+//         try {
+//             throw new AgeException("Age must be 18 or above to vote.");
+//         } catch (AgeException e) {
+//             System.out.println("Caught: " + e.getMessage());
+//         }
+//     }
+// }
+
+
 
 public class CustomExcProgram {
     public static void main(String[] args) {
         try {
-            throw new AgeException("Age must be 18 or above to vote.");
-        } catch (AgeException e) {
+            throw new ArithmeticException("Exception");
+        } catch (ArithmeticException e) {
             System.out.println("Caught: " + e.getMessage());
         }
     }
